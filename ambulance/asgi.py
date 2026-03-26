9da8c1dec53c79db3dd25b823025bb28bc0b83dc
om-pujari <om-pujari>
2026-03-26 17:08:50 +0530

    debug asgi render

--- ambulance/asgi.py
+++ ambulance/asgi.py
@@ -7,18 +7,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ambulance.settings")
+
 from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from core import routing
-import django
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ambulance.settings")
-django.setup()
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(routing.websocket_urlpatterns)
     ),
